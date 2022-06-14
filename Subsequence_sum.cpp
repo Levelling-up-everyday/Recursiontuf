82f38e726ff8f47b9029ee0af1e29d9e34e6307f
Levelling-up-everyday <106878532+Levelling-up-everyday@users.noreply.github.com>
2022-06-14 19:19:02 +0530

    Update Subsequence_sum.cpp

--- Subsequence_sum.cpp
+++ Subsequence_sum.cpp
@@ -33,3 +33,66 @@ int main()
    powerset(arr,ans,0,3,2,0);
     return 0;
 }
+
+
+//TO print only one subsequence
+
+bool powerset(int arr[], vector<int> &ans, int index ,int n, int sum, int s)
+{
+
+    if(index==n)
+    {
+        if(s==sum)
+        {
+            for(auto it:ans)
+            cout<<it<<" ";
+            cout<<endl;
+            return true;
+        }
+        
+        return false;
+    }
+    
+    ans.push_back(arr[index]);
+    s+=arr[index];
+    if(powerset(arr,ans,index+1,n,sum,s)==true) return true;
+    ans.pop_back();
+    s-=arr[index];
+    if(powerset(arr,ans,index+1,n,sum,s)==true) return true;
+    return false;
+}
+//Same main functiona as above
+
+
+
+//TO count the no of subsequence whose sum is given
+
+#include<bits/stdc++.h>
+using namespace std;
+int powerset(int arr[], int index ,int n, int sum, int s)
+{
+
+    if(index==n)
+    {
+        if(s==sum)
+        {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    s+=arr[index];
+    int l=powerset(arr,index+1,n,sum,s);
+
+    s-=arr[index];
+    int r=powerset(arr,index+1,n,sum,s);
+    
+    return l+r;
+}
+int main()
+{
+   int arr[]={1,2,1};
+   cout<<powerset(arr,0,3,2,0);
+    return 0;
+}
